63990682d3e0dae0b9a15fb124690c95ba6f1dbf
Jake Sorce <jakesorce@gmail.com>
2016-06-09 15:30:01 -0600

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-
-  devise_for :users
   root 'landings#index'
-  get 'bio', to: 'landings#bio'
-  resources :photos, except: [:show, :new]
+  devise_for :users
   
-  #->Prelang (user_login:devise/stylized_paths)
-  devise_scope :user do
-    get    "login"   => "users/sessions#new",         as: :new_user_session
-    post   "login"   => "users/sessions#create",      as: :user_session
-    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
-    
-    get    "signup"  => "users/registrations#new",    as: :new_user_registration
-    post   "signup"  => "users/registrations#create", as: :user_registration
-    put    "signup"  => "users/registrations#update", as: :update_user_registration
-    get    "account" => "users/registrations#edit",   as: :edit_user_registration
-  end
+  # GET Routes
+  get 'bio', to: 'landings#bio'
 
+  # Resources Routes
+  resources :photos, except: [:show, :new]
 end
